54d83e92ae75437f161f4e6a5de1fc131da6ab0d
Lorenzo Ferrara Junior <lorenzoferrarajr@gmail.com>
2012-10-24 01:51:09 +0200

    Added setters and getters for login and callback url

--- src/LfjOpauth/Service/OpauthService.php
+++ src/LfjOpauth/Service/OpauthService.php
@@ -10,6 +10,8 @@ class OpauthService implements ServiceLocatorAwareInterface
     private $serviceLocator;
     private $options;
     private $router;
+    private $loginUrlName;
+    private $callbackUrlName;
 
     public function redirect($provider, $oauth_callback)
     {
@@ -70,10 +72,34 @@ class OpauthService implements ServiceLocatorAwareInterface
             $this->setOptions($this->getServiceLocator()->get('lfjopauth_module_options'));
         }
 
-        $this->options['path'] = $this->getRouter()->assemble(array(), array('name' => 'lfjopauth_login'));
-        $this->options['callback_url'] = $this->getRouter()->assemble(array('provider' => $provider), array('name' => 'lfjopauth_callback'));
+        $this->options['path'] = $this->getRouter()->assemble(array(), array('name' => $this->getLoginUrlName()));
+        $this->options['callback_url'] = $this->getRouter()->assemble(array('provider' => $provider), array('name' => $this->getCallbackUrlName()));
 
         return $this->options;
+    }
+
+    public function setLoginUrlName($loginUrlName)
+    {
+        $this->loginUrlName = $loginUrlName;
+    }
+
+    public function getLoginUrlName()
+    {
+        if ($this->loginUrlName == null) return 'lfjopauth_login';
+
+        return $this->loginUrlName;
+    }
+
+    public function setCallbackUrlName($callbackUrlName)
+    {
+        $this->callbackUrlName = $callbackUrlName;
+    }
+
+    public function getCallbackUrlName()
+    {
+        if ($this->callbackUrlName == null) return 'lfjopauth_callback';
+
+        return $this->callbackUrlName;
     }
 
     public function getServiceLocator()
