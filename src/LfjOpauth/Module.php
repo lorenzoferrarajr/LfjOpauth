a37bb0405c3330b1476b009b3f5a5011ba3f87b2
Lorenzo Ferrara <lorenzoferrarajr@gmail.com>
2013-09-15 13:24:04 +0200

    Updated composer.json and Module.php
    - upgraded to Opauth 0.4.4
    - now using autoload psr-0 namespace mapping
    - changed position of Module.php code to reflect composer.json changes

---
+++ src/LfjOpauth/Module.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace LfjOpauth;
+
+use Zend\ModuleManager\ModuleManager;
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+use Zend\ModuleManager\Feature\ServiceProviderInterface;
+use Zend\EventManager\EventInterface as Event;
+use Zend\Session\SessionManager;
+
+class Module implements ConfigProviderInterface, ServiceProviderInterface
+{
+    public function getServiceConfig()
+    {
+        return array(
+            'factories' => array(
+                'lfjopauth_module_options' => function ($sm) {
+                    $config = $sm->get('Config');
+
+                    return isset($config['lfjopauth']) ? $config['lfjopauth'] : array();
+                },
+                'opauth_service' => function($sm) {
+                    $opauth = new \LfjOpauth\Service\OpauthService();
+                    $router = $sm->get('router');
+                    $opauth->setRouter($router);
+
+                    return $opauth;
+                }
+             )
+        );
+    }
+
+    public function onBootstrap(Event $e)
+    {
+         $session = new SessionManager();
+         if (!$session->sessionExists()) $session->start();
+    }
+
+    public function getConfig($env = null)
+    {
+        return include __DIR__ . '/../../config/module.config.php';
+    }
+}
